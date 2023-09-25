11fd457c332622e1d32234e80951fad0e6734a69
wenxuw <xu_wen@sfu.ca>
2023-09-25 16:54:27 -0700

    start up the server

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
-const expr = require("express");
-const app = expr();
-app.listen(3000);
+import express, { Request, Response, NextFunction } from "express";
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello from the server side!");
+});
+
+const port: number = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
