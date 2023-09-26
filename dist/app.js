6207dc44966ad60e1f5cee0d69bde1642e17fc30
wenxuw <xu_wen@sfu.ca>
2023-09-26 14:35:32 -0700

    post request for creating a new event

--- dist/app.js
+++ dist/app.js
@@ -4,9 +4,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.status(200).send("Hello from the server side!");
+app.use(express_1.default.json());
+// app.get("/", (req: Request, res: Response) => {
+//   res
+//     .status(200)
+//     .send({ message: "Hello from the server side!", app: "GatherIn" });
+// });
+// app.post("/", (req: Request, res: Response) => {
+//   res.send("You can post to this endpoint!");
+// });
+const events = JSON.parse(fs_1.default.readFileSync(`${__dirname}/dev-data/data/events-simple.json`).toString());
+app.get("/api/v1/events", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        results: events.length,
+        data: { events },
+    });
+});
+app.post("/api/v1/events", (req, res) => {
+    const newId = events[events.length - 1].id + 1;
+    const newEvent = Object.assign({ id: newId }, req.body);
+    events.push(newEvent);
+    fs_1.default.writeFile(`${__dirname}/dev-data/data/events-simple.json`, JSON.stringify(events), (err) => {
+        res.status(201).json({
+            status: "success",
+            data: { events: newEvent },
+        });
+    });
 });
 const port = 3000;
 app.listen(port, () => {
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,64 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, {
+  Request,
+  Response,
+  NextFunction,
+  RequestHandler,
+} from "express";
+import fs from "fs";
+
+interface Event {
+  id: number;
+  name: string;
+  location: string;
+  startingTime: string;
+  endingTime: string;
+  size: number;
+  charge: number;
+  organizer: string;
+  description: string;
+  imageCover: string;
+  images: string[];
+}
+
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Hello from the server side!");
+app.use(express.json());
+
+// app.get("/", (req: Request, res: Response) => {
+//   res
+//     .status(200)
+//     .send({ message: "Hello from the server side!", app: "GatherIn" });
+// });
+
+// app.post("/", (req: Request, res: Response) => {
+//   res.send("You can post to this endpoint!");
+// });
+
+const events: Event[] = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/events-simple.json`).toString()
+);
+app.get("/api/v1/events", (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "success",
+    results: events.length,
+    data: { events },
+  });
+});
+
+app.post("/api/v1/events", (req: Request, res: Response) => {
+  const newId: number = events[events.length - 1].id! + 1;
+  const newEvent: Event = Object.assign({ id: newId }, req.body);
+  events.push(newEvent);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/events-simple.json`,
+    JSON.stringify(events),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: { events: newEvent },
+      });
+    }
+  );
 });
 
 const port: number = 3000;
