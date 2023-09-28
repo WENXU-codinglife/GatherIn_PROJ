e26e4e11d1090d116f1746eaa685c5981802f894
wenxuw <xu_wen@sfu.ca>
2023-09-28 16:07:43 -0700

    db connection

--- dist/server.js
+++ dist/server.js
@@ -3,9 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config({ path: `${__dirname}/../config.env` });
+const utils_1 = require("./utils/utils");
 const app_1 = __importDefault(require("./app"));
+dotenv_1.default.config({ path: `${__dirname}/../config.env` });
+const DATABASE_URI = (0, utils_1.currentMode)("dev")
+    ? process.env.DATABASE_DEV
+    : process.env.DATABASE;
+const db = DATABASE_URI.replace("<password>", process.env.DATABASE_PASSWORD);
+mongoose_1.default.connect(db).then((con) => {
+    console.log("DB connection successful!");
+});
 const port = +process.env.PORT || 3000;
 app_1.default.listen(port, () => {
     console.log(`App running on port ${port}...`);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { currentMode } from "./utils/utils";
+import app from "./app";
+
 dotenv.config({ path: `${__dirname}/../config.env` });
 
-import app from "./app";
+const DATABASE_URI = currentMode("dev")
+  ? process.env.DATABASE_DEV!
+  : process.env.DATABASE!;
+const db = DATABASE_URI.replace("<password>", process.env.DATABASE_PASSWORD!);
+
+mongoose.connect(db).then((con) => {
+  console.log("DB connection successful!");
+});
 
 const port: number = +process.env.PORT! || 3000;
 app.listen(port, () => {
