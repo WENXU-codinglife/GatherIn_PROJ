d69b41f0ae9b59d24c4a66420fe85440a4e8c11b
wenxuw <xu_wen@sfu.ca>
2023-10-04 16:45:25 -0700

    implement validation

--- dist/models/eventModel.js
+++ dist/models/eventModel.js
@@ -9,22 +9,52 @@ const eventSchema = new mongoose_1.default.Schema({
         type: String,
         trim: true,
         required: [true, "An event must have a name!"],
+        maxLength: [
+            40,
+            "An event name must have less or equal than 40 characters",
+        ],
+        minLength: [
+            5,
+            "An event name must have greater or equal than 5 characters",
+        ],
+        // validate: [
+        //   validator.isAlphanumeric,
+        //   "Event names can only have letters and numbers!",
+        // ],
     },
     location: {
         type: String,
         require: false,
     },
-    startingTime: Date,
-    endingTime: Date,
+    startingTime: {
+        type: Date,
+        require: true,
+        min: [Date.now(), "The event starting time should be in the future!"],
+    },
+    endingTime: { type: Date, require: true },
     createdAt: {
         type: Date,
         default: Date.now(),
     },
-    size: { type: Number, default: 0 },
-    charge: Number,
+    size: {
+        type: Number,
+        default: 0,
+        min: [0, "Size must be above 0!"],
+        // validate: {
+        //   validator: function (this: IEvent, size: number) {
+        //     const q = this;
+        //     console.log(q);
+        //     console.log(this.get("name"));
+        //     return size <= this.maxGroupSize;
+        //   },
+        //   message: (props) => `props ========> ${props.value}`,
+        // },
+    },
+    charge: { type: Number, min: [0, "A charge must be above 0!"] },
     maxGroupSize: {
         type: Number,
         default: 0,
+        min: [0, "Size must be above 0!"],
     },
     organizer: {
         type: String,
@@ -41,6 +71,10 @@ const eventSchema = new mongoose_1.default.Schema({
     images: {
         type: Array(String),
     },
+    isPrivate: {
+        type: Boolean,
+        default: false,
+    },
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
@@ -48,6 +82,5 @@ const eventSchema = new mongoose_1.default.Schema({
 eventSchema.virtual("availablePositions").get(function () {
     return this.maxGroupSize - this.size;
 });
-// DOCUMENT MIDDLEWARE: runs only before .save() and .create()
 const Event_Model = mongoose_1.default.model("Events", eventSchema);
 exports.default = Event_Model;
--- src/models/eventModel.ts
+++ src/models/eventModel.ts
@@ -1,26 +1,75 @@
 import mongoose from "mongoose";
-const eventSchema = new mongoose.Schema(
+import validator from "validator";
+interface IEvent extends mongoose.Document {
+  _id: mongoose.Schema.Types.ObjectId;
+  name: string;
+  location: string;
+  startingTime: Date;
+  endingTime: Date;
+  createdAt: Date;
+  size: number;
+  maxGroupSize: number;
+  charge?: number;
+  organizer: string;
+  description?: string;
+  imageCover: string;
+  images?: string[];
+  isPrivate: boolean;
+}
+
+const eventSchema = new mongoose.Schema<IEvent>(
   {
     name: {
       type: String,
       trim: true,
       required: [true, "An event must have a name!"],
+      maxLength: [
+        40,
+        "An event name must have less or equal than 40 characters",
+      ],
+      minLength: [
+        5,
+        "An event name must have greater or equal than 5 characters",
+      ],
+      // validate: [
+      //   validator.isAlphanumeric,
+      //   "Event names can only have letters and numbers!",
+      // ],
     },
     location: {
       type: String,
       require: false,
     },
-    startingTime: Date,
-    endingTime: Date,
+    startingTime: {
+      type: Date,
+      require: true,
+      min: [Date.now(), "The event starting time should be in the future!"],
+    },
+    endingTime: { type: Date, require: true },
     createdAt: {
       type: Date,
       default: Date.now(),
     },
-    size: { type: Number, default: 0 },
-    charge: Number,
+    size: {
+      type: Number,
+      default: 0,
+      min: [0, "Size must be above 0!"],
+      // validate: {
+      //   validator: function (this: IEvent, size: number) {
+      //     const q = this;
+
+      //     console.log(q);
+      //     console.log(this.get("name"));
+      //     return size <= this.maxGroupSize;
+      //   },
+      //   message: (props) => `props ========> ${props.value}`,
+      // },
+    },
+    charge: { type: Number, min: [0, "A charge must be above 0!"] },
     maxGroupSize: {
       type: Number,
       default: 0,
+      min: [0, "Size must be above 0!"],
     },
     organizer: {
       type: String,
@@ -37,6 +86,10 @@ const eventSchema = new mongoose.Schema(
     images: {
       type: Array(String),
     },
+    isPrivate: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -44,11 +97,9 @@ const eventSchema = new mongoose.Schema(
   }
 );
 
-eventSchema.virtual("availablePositions").get(function () {
+eventSchema.virtual("availablePositions").get(function (): number {
   return this.maxGroupSize - this.size;
 });
 
-// DOCUMENT MIDDLEWARE: runs only before .save() and .create()
-
-const Event_Model = mongoose.model("Events", eventSchema);
+const Event_Model = mongoose.model<IEvent>("Events", eventSchema);
 export default Event_Model;
