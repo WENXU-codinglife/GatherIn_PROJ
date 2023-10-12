3577aa63c30805bd99483a432787f5b566d35267
wenxuw <xu_wen@sfu.ca>
2023-10-11 17:04:19 -0700

    secure auth

--- dist/controllers/authController.js
+++ dist/controllers/authController.js
@@ -4,12 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.signup = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const catchAsync_1 = __importDefault(require("../utils/catchAsync"));
 const userModel_1 = __importDefault(require("./../models/userModel"));
 exports.signup = (0, catchAsync_1.default)(async (req, res, next) => {
-    const newUser = await userModel_1.default.create(req.body);
+    // const newUser = await User_Model.create(req.body);
+    const newUser = await userModel_1.default.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
+    const token = jsonwebtoken_1.default.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
     res.status(201).json({
         status: "success",
-        data: newUser,
+        data: { user: newUser },
+        token,
     });
 });
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,25 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 import catchAsync from "../utils/catchAsync";
 import User_Model from "./../models/userModel";
 
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const newUser = await User_Model.create(req.body);
+    // const newUser = await User_Model.create(req.body);
+    const newUser = await User_Model.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET!, {
+      expiresIn: process.env.JWT_EXPIRES_IN!,
+    });
     res.status(201).json({
       status: "success",
-      data: newUser,
+      data: { user: newUser },
+      token,
     });
   }
 );
